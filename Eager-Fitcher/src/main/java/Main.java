4624a006dce49e9003dca98b2c0829fe3c11bd73
RandikaEdirisooriya <randikaedirisooriya@gmail.com>
2024-02-09 09:41:48 +0530

    Eager fitching

--- Eager-Fitcher/src/main/java/Main.java
+++ Eager-Fitcher/src/main/java/Main.java
@@ -9,38 +9,33 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
-      /*  student.setId("S001");
+        Student student=new Student();
+        student.setId("11e");
         student.setName("Randika");
         student.setAddress("Medagama");
 
 
         Laptop laptop1=new Laptop();
-        laptop1.setId("I001");
-        laptop1.setModel("Asus");
+        laptop1.setId("10111");
+        laptop1.setModel("woow");
         laptop1.setStudent(student);
         Laptop laptop2=new Laptop();
-        laptop2.setId("I002");
-        laptop2.setModel("dell");
+        laptop2.setId("1011111");
+        laptop2.setModel("woo1");
         laptop2.setStudent(student);
 
         List<Laptop> laptops=new ArrayList<>();
         laptops.add(laptop1);
-        laptops.add(laptop2);*/
+        laptops.add(laptop2);
 
         Session session= FactoryConfiguration.getInstance().getSession();
-      Transaction transaction=session.beginTransaction();
-       // session.save(student);
-       /* for (Laptop laptop : laptops) {
-            session.save(laptop);*/
-
-        Student student=session.get(Student.class,"S001");
-        String student1= student.getName();
-        System.out.println(student1);
+        Transaction transaction=session.beginTransaction();
+        session.save(student);
+        for (Laptop laptop : laptops) {
+            session.save(laptop);
+        }
 
         transaction.commit();
         session.close();
-
     }
 }
